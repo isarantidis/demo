acc06e7f5bf87fd3602de41e8f13d6edc8823aa0
u13029 <u13029@W-L-2012-01614.echa.europa.local>
2017-04-26 16:30:36 +0300

    end

---
+++ src/main/java/com/example/Endpoint.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Endpoint {
+
+	@GetMapping(path="/get")
+	public HttpEntity<String> get() {
+		return new HttpEntity<String>("dsa");
+	}
+}
